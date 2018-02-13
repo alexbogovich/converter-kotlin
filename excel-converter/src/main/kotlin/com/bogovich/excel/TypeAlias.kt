f34d96d6de5b40e3e03a437fb9d677d2c7a939d2
Alexander Bogovich <bogovich.alexander@gmail.com>
2018-02-13 12:25:15 +0700

    Remove poi

--- excel-converter/src/main/kotlin/com/bogovich/excel/TypeAlias.kt
+++ excel-converter/src/main/kotlin/com/bogovich/excel/TypeAlias.kt
@@ -2,7 +2,4 @@ package com.bogovich.excel
 
 import com.bogovich.xml.writer.dsl.DslXMLStreamWriter
 
-typealias WriterWithCursor = DslXMLStreamWriter.(cursor: Cursor) -> Unit
-typealias CheckStatement = (cursor: Cursor) -> Boolean
-typealias CursorOperation = (cursor: Cursor) -> Unit
 typealias RowDataCallback = suspend (rowData: RowData) -> Unit
