75070131b40e46b78d9f9ed4bfb340ce5169d058
Alexander Bogovich <bogovich.alexander@gmail.com>
2018-02-14 13:19:06 +0700

    Refactor reader

--- excel-converter/src/main/kotlin/com/bogovich/excel/Reader.kt
+++ excel-converter/src/main/kotlin/com/bogovich/excel/Reader.kt
@@ -8,19 +8,18 @@ import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Paths
 
-class Reader (private val mainFileChannel: Channel<RowData>, private val restFileChannel: Channel<RowData>) {
+class Reader(private val mainFileChannel: Channel<RowData>, private val restFileChannel: Channel<RowData>) {
 
     companion object : KLogging()
 
-    suspend fun readMainDoc(path: String) {
+    suspend fun read(path: String, channel: Channel<RowData>) {
         val inputStream = Files.newInputStream(Paths.get(path))
         inputStream.use { stream ->
-            readMainDoc(stream)
+            read(stream, channel)
         }
-        mainFileChannel.close()
     }
 
-    suspend fun readMainDoc(inputStream: InputStream) {
+    suspend fun read(inputStream: InputStream, channel: Channel<RowData>) {
         val workbook = StreamingReader.builder()
                 .rowCacheSize(100)    // number of rows to keep in memory (defaults to 10)
                 .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
@@ -36,36 +35,19 @@ class Reader (private val mainFileChannel: Channel<RowData>, private val restFil
                         .also { cells ->
                             logger.info { "prepare to send row ${row.rowNum}" }
                             logger.info { "Send $cells" }
-                            mainFileChannel.send(RowData(sheetNum + 1, row.rowNum, cells))
+                            channel.send(RowData(sheetNum + 1, row.rowNum, cells))
                             logger.info { "Sent ${row.rowNum}" }
                         }
             }
         }
     }
 
+    suspend fun readMainDoc(path: String) {
+        read(path, mainFileChannel)
+        mainFileChannel.close()
+    }
+
     suspend fun readRestDocs(path: String) {
-        val inputStream = Files.newInputStream(Paths.get(path))
-        inputStream.use { stream ->
-            val workbook = StreamingReader.builder()
-                    .rowCacheSize(100)    // number of rows to keep in memory (defaults to 10)
-                    .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
-                    .open(stream)            // InputStream or File for XLSX file (required)
-            for ((sheetNum, sheet) in workbook.withIndex()) {
-//                println(sheet.sheetName)
-                for (row in sheet) {
-//                    println("read row ${row.rowNum}")
-                    row.asSequence()
-                            .filter { cell: Cell -> !cell.stringCellValue.isNullOrEmpty() }
-                            .map { cell: Cell -> CellData.of(cell) }
-                            .associateBy { cellData: CellData -> cellData.ref }
-                            .also { cells ->
-                                logger.info { "prepare to send row ${row.rowNum}" }
-                                logger.info { "Send $cells" }
-                                restFileChannel.send(RowData(sheetNum + 1, row.rowNum, cells))
-                                logger.info { "Sent ${row.rowNum}" }
-                            }
-                }
-            }
-        }
+        read(path, restFileChannel)
     }
 }
--- excel-converter/src/main/kotlin/com/bogovich/excel/TypeAlias.kt
+++
@@ -1,5 +0,0 @@
-package com.bogovich.excel
-
-import com.bogovich.xml.writer.dsl.DslXMLStreamWriter
-
-typealias RowDataCallback = suspend (rowData: RowData) -> Unit
